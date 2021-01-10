732bbfc1e01e205cf857b8a8c539e3347b8643eb
openzource <yasna.romann@gmail.com>
2021-01-09 22:37:27 -0300

    cambios de textos

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Learn to design and code Reacts apps</h1>
+    <p>Diseño de aplicaicones moviles :)</p>
+    <p>revisa aquí que te parece.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/page-2/">Conocer más</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
